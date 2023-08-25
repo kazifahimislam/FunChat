af3749a75fdc77979d7ace66f70799fbdcce422e
[200~FAHIMBHAI786~ <fahimislam7860@gmail.com>
2023-08-25 12:14:22 +0530

    firebase

--- app/src/main/java/com/example/funchat/MainActivity.kt
+++ app/src/main/java/com/example/funchat/MainActivity.kt
@@ -3,29 +3,48 @@ package com.example.funchat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var send : Button
     private lateinit var textview : TextView
+    private lateinit var email : EditText
+    private lateinit var password : EditText
+
+    private lateinit var mAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         send = findViewById(R.id.send)
         textview = findViewById(R.id.newAccount)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
 
-        send.setOnClickListener{
+        mAuth = FirebaseAuth.getInstance()
 
-        }
         textview.setOnClickListener{
             val intent = Intent(this, Otp::class.java)
             startActivity(intent)
         }
+        send.setOnClickListener{
+            val email = email.text.toString()
+            val password = password.text.toString()
+
+            login(email,password);
+
         }
 
 
+        }
+
+    private fun login (email: String, password : String){
+
+    }
 
     }
--- app/src/main/java/com/example/funchat/Otp.kt
+++ app/src/main/java/com/example/funchat/Otp.kt
@@ -4,18 +4,64 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class Otp : AppCompatActivity() {
     private lateinit var button : Button
+    private lateinit var name : EditText
+    private lateinit var email : EditText
+    private lateinit var password : EditText
+
+    private lateinit var mAuth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
 
         button = findViewById(R.id.sendOtp)
+        name = findViewById(R.id.name)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        
 
         button.setOnClickListener{
-            val intent = Intent(this, home::class.java)
-            startActivity(intent)
+
+
+            val email = email.text.toString()
+            val password = password.text.toString()
+            val name = name.text.toString()
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task: Task<AuthResult?> ->
+                    if (task.isSuccessful) {
+
+                        val user = mAuth.currentUser
+                        val profileUpdates = UserProfileChangeRequest.Builder()
+                            .setDisplayName(name)
+                            .build()
+
+                        user?.updateProfile(profileUpdates)
+                            ?.addOnCompleteListener { profileTask: Task<Void?> ->
+                                if (profileTask.isSuccessful) {
+                                    val intent = Intent(this, home::class.java)
+                                    startActivity(intent)
+
+                                } else {
+
+
+                                }
+                            }
+                    } else {
+
+                    }
+                }
         }
     }
 }
