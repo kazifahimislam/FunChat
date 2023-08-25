91dc371c05c7ae0f1ad67a7514e1885faedc90c0
[200~FAHIMBHAI786~ <fahimislam7860@gmail.com>
2023-08-25 12:12:58 +0530

    firebase

--- app/src/main/java/com/example/funchat/MainActivity.kt
+++ app/src/main/java/com/example/funchat/MainActivity.kt
@@ -4,20 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var send : Button
+    private lateinit var textview : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         send = findViewById(R.id.send)
+        textview = findViewById(R.id.newAccount)
 
         send.setOnClickListener{
+
+        }
+        textview.setOnClickListener{
             val intent = Intent(this, Otp::class.java)
             startActivity(intent)
         }
         }
+
+
 
     }
