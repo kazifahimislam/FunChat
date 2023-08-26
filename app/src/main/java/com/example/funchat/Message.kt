936175a4556f1deaf1739e1cae5957a12f83ce88
[200~FAHIMBHAI786~ <fahimislam7860@gmail.com>
2023-08-26 23:55:05 +0530

    final chat activity

--- app/src/main/java/com/example/funchat/Message.kt
+++ app/src/main/java/com/example/funchat/Message.kt
@@ -1,5 +1,16 @@
 package com.example.funchat
 
+import android.content.IntentSender
+
 class Message {
-    var messa
+    var message: String? = null
+    var senderId: String? = null
+
+    constructor(){}
+
+    constructor(message: String, senderId : String?){
+        this.message = message
+        this.senderId = senderId
+
+    }
 }
--- app/src/main/java/com/example/funchat/UserAdapter.kt
+++ app/src/main/java/com/example/funchat/UserAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.funchat
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.NonDisposableHandle.parent
 
 class UserAdapter(val context: Context, val userList: ArrayList<User>): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
@@ -25,6 +27,16 @@ class UserAdapter(val context: Context, val userList: ArrayList<User>): Recycler
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val currentUser = userList[position]
         holder.textName.text = currentUser.name
+
+
+        holder.itemView.setOnClickListener{
+            val intent = Intent(context,ChatActivity::class.java)
+            intent.putExtra("name", currentUser.name)
+            intent.putExtra("uid", currentUser.uid)
+
+            context.startActivity(intent)
+        }
+
     }
     class UserViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
