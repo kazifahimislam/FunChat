28a3b3ed49e9d1a8322ff33e48380f4070a76217
[200~FAHIMBHAI786~ <fahimislam7860@gmail.com>
2023-08-29 21:49:33 +0530

    Ui change, splash screen update

--- app/src/main/java/com/example/funchat/SplashScreenActivity.kt
+++ app/src/main/java/com/example/funchat/SplashScreenActivity.kt
@@ -4,16 +4,25 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.google.firebase.auth.FirebaseAuth
 
 class SplashScreenActivity : AppCompatActivity() {
 
+    private val SPLASH_SCREEN_DURATION = 1000 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        // Delay the execution of other tasks by 1 second
+        Handler(Looper.getMainLooper()).postDelayed({
+            executeTasks()
+        }, SPLASH_SCREEN_DURATION.toLong())
+    }
 
+    private fun executeTasks() {
         val sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
         val lastActivity = sharedPref.getString("lastActivity", "Home")
 
@@ -31,6 +40,6 @@ class SplashScreenActivity : AppCompatActivity() {
         }
 
         startActivity(intent)
-        finish() // Close the splash screen activity
+        finish()
     }
 }
