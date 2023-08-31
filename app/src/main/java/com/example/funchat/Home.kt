1e54a17259964be90a239172a3b313bb1508d8cc
[200~FAHIMBHAI786~ <fahimislam7860@gmail.com>
2023-08-31 19:59:33 +0530

    Google authentication added

--- app/src/main/java/com/example/funchat/Home.kt
+++ app/src/main/java/com/example/funchat/Home.kt
@@ -10,7 +10,6 @@ import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
